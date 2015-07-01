c63c15ab5f91b2941d0b6122795d696046ef4c3e
Samaed <corentin.haidon@isen-lille.fr>
2015-07-01 14:28:07 -0400

    DiagnosticService retrieve Disease[] from server. Add some error handling. Add the Service to Activity communication. Make Disease and Symptom models common to REST API and Android, make Disease Parcelable

--- app/src/main/java/org/samaed/aae/Symptom.java
+++ app/src/main/java/org/samaed/aae/Symptom.java
@@ -4,15 +4,16 @@ public class Symptom  {
     protected static final String DEFAULT_NAME = "Symptom";
     protected static final float DEFAULT_VALUE = 0f;
     protected String uniqueName;
-    protected float value;
+    protected float min, max;
 
     public Symptom() {
-        this(DEFAULT_NAME, DEFAULT_VALUE);
+        this(DEFAULT_NAME, DEFAULT_VALUE, DEFAULT_VALUE);
     }
 
-    public Symptom(String name, float value) {
+    public Symptom(String name, float min, float max) {
         this.uniqueName = name;
-        this.value = value;
+        this.min = min;
+        this.max = max;
     }
 
     public void setUniqueName(String name) {
@@ -23,16 +24,29 @@ public class Symptom  {
         return this.uniqueName;
     }
 
-    public void setValue(float value) {
-        this.value = value;
+    public void setMin(float value) {
+        this.min = value;
     }
 
-    public float getValue() {
-        return this.value;
+    public float getMin() {
+        return this.min;
+    }
+
+    public void setMax(float value) {
+        this.max = value;
+    }
+
+    public float getMax() {
+        return this.max;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return (object instanceof Symptom) && ((Symptom)object).getMin() >= getMin() && ((Symptom)object).getMax() <= getMax();
     }
 
     @Override
     public String toString() {
-        return String.format("Symptom : %s@%f",getUniqueName(),getValue());
+        return String.format("Symptom : %s:[%f,%f]",getUniqueName(),getMin(),getMax());
     }
 }
